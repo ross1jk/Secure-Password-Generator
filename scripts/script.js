ffedc3c317c1630b57eba5bae80710d4538e826d
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-10 21:54:21 -0500

    when alerted, a random lowercase letter will be generated. currently no matter what option is given this will happen. will work on updating.

--- scripts/script.js
+++ scripts/script.js
@@ -12,14 +12,31 @@ function writePassword() {
 
 //Password Criteria through prompt:
 //8 - 128 charcters = length
+
+//var length
+
 //Character Types: lowercase upper case numeric special 
+//var characters = [lowercase, uppercase, numeric]
+  lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 
-//will eventually use a while loop, so that when a user wants to stop, they can but only if the yare between 8 and 128 'alert must be between'
+  //will eventually use a while loop, so that when a user wants to stop, they can but only if the yare between 8 and 128 'alert must be between'
 
-prompt("Choose a character type: lowercase, uppercase, numeric, or special");
+//only giving me lowercase console.log is givng me random letters tho
+ prompt("Choose a character type: lowercase, uppercase, numeric, or special");
   if ("lowercase") {
-    prompt("select a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, or z");
+    console.log(lowercase[Math.floor(Math.random() * lowercase.length)]);
   }
+  else if ("uppercase") {
+    console.log(uppercase[Math.floor(Math.random() * uppercase.length)]);
+  }
+  else if ("numeric") {
+    console.log(numeric[Math.floor(Math.random() * numeric.length)]);
+  }
+  //else if ("special") { come back to and figure out how to write these in a string
+    //prompt("choose: !#$%&'(*+,-./:;<=>?@[\]^_`{|}~"); 
+  //}
 
   
 
