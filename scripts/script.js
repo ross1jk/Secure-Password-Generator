ac74c0aa31e83f93b9b1bbce5c952f22c2d6c5e7
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-10 20:27:01 -0500

    started some psuedo code on what I will need the user to answer in javascirpt. Completed first prompt. prompt is not logged.

--- scripts/script.js
+++ scripts/script.js
@@ -10,5 +10,18 @@ function writePassword() {
 
 }
 
+//Password Criteria through prompt:
+//8 - 128 charcters = length
+//Character Types: lowercase upper case numeric special 
+
+//will eventually use a while loop, so that when a user wants to stop, they can but only if the yare between 8 and 128 'alert must be between'
+
+prompt("Choose a character type: lowercase, uppercase, numeric, or special");
+  if ("lowercase") {
+    prompt("select a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, or z");
+  }
+
+  
+
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
