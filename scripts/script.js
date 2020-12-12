e03679f025087a1caed04ec54f2b5c3aa2f3c3d9
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-12 14:04:58 -0500

    can generate random characters if selected. and rewrite the stats. working on logging complete passwrod. added more psuedo code

--- scripts/script.js
+++ scripts/script.js
@@ -9,81 +9,64 @@ function writePassword() {
   passwordText.value = password;
 
  }
-
-  //var passwordChar = [];
-  //for (var i = 8; 8<= N;) {
- //   passwordChar.push(i)
-  //}
+   //all this needs to go into generate password besides varibles 
  
   //Varibles 
-  var passwordChar = prompt("How many characters do you want your password to have (must be between 8 - 128)?"); 
+  var completePassword = "";
+  var passwordLength = 0;
+  var chooseLength = prompt("How many characters do you want your password to have (must be between 8 - 128)?"); 
   var confirmLowercase = confirm("Do you want lowercase letteres?");
   var confirmUppercase = confirm("Do you want uppercase letters?");
   var confirmNumeric = confirm("Do you want numeric characters?");
   var confirmSpecial = confirm("Do you want special charactesr?");
 
-  lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  special = ["~", "%"]
+  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+  var special = ["~", "%"]
 
-  
- //Password Criteria through prompt:
- //How many characters do you want 8 - 128 charcters = length
-   
-    if (passwordChar <= 7) {
-      alert("Your password length does not meet security requirements!");
-    }
-    else if (passwordChar >= 129) {
+
+  // function 
+  function reWriteStats() {
+    console.log(lowercase[Math.floor(Math.random() * lowercase.length)]);
+    console.log(uppercase[Math.floor(Math.random() * uppercase.length)]);
+    console.log(numeric[Math.floor(Math.random() * numeric.length)]);
+    console.log(special[Math.floor(Math.random() * special.length)]);
+  }
+    //Password Lenght Choices:
+    if (chooseLength < 8 && chooseLength > 128) {
       alert("Your password length does not meet security requirements!");
     }
     else {
-      console.log(passwordChar); 
-    }
- 
-  //need conditional
- //Do you want lowercase
-  
-   if (confirmLowercase) {
-    console.log(lowercase[Math.floor(Math.random() * lowercase.length)]);
-   }
-   //need conditional
-   //Do you want uppercase
-  
-    if (confirmUppercase) {
-      console.log(uppercase[Math.floor(Math.random() * uppercase.length)]);
-    }
-   //need conditional
-   //Do you want numeric 
-  
-    if (confirmNumeric) {
-      console.log(numeric[Math.floor(Math.random() * numeric.length)]);
-    }
-    
-    //need conditional
-    //Do you want special 
-  
-    if (confirmSpecial) {
-      console.log(special[Math.floor(Math.random() * special.length)]);
-    }
-  //need conditional 
+      chooseLength = passwordLength
+    } 
 
+   // "drewjason" + "cat" -- want to do contintinously while adding a letter 
+  completePassword = lowercase[Math.floor(Math.random() * lowercase.length)] + uppercase[Math.floor(Math.random() * uppercase.length)];
+  console.log(completePassword);
 
+   //Password Criteria through prompt:
+      //can use an array to randomize this
+      //for or while loop to complete 
+        if (confirmLowercase) {
+          lowercase = reWriteStats();
+        }
+        else if (confirmUppercase) {
+          uppecase = reWriteStats();
+        }
+        else if (confirmNumeric) {
+          numeric = reWriteStats();
+        }
+        else if (confirmSpecial) {
+          speical = reWriteStats();
+        }
+     // }
 
-
-  //Character Types: lowercase upper case numeric special 
-  //var characters = [lowercase, uppercase, numeric]
- 
-
-  //will eventually use a while loop, so that when a user wants to stop, they can but only if the yare between 8 and 128 'alert must be between'
-
-  //only giving me lowercase console.log is givng me random letters tho
- //prompt("Choose a character type: lowercase, uppercase, numeric, or special");
- 
-  //}
-  //else if ("special") { come back to and figure out how to write these in a string
-    //prompt("choose: !#$%&'(*+,-./:;<=>?@[\]^_`{|}~"); 
-  //}
+    //Character Lengths
+     
+    //add all together 
+   
+    //document.getElementById
 
   
 
