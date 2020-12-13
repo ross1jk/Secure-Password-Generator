eb272d644531e96d0580c8e6ee375f6d41583d83
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-13 11:20:03 -0500

    created the correct length for generate password. Need to work on null values

--- scripts/script.js
+++ scripts/script.js
@@ -8,51 +8,55 @@ function writePassword() {
 
   passwordText.value = password;
 }
-   
- //all this needs to go into generate password besides varibles 
- function generatePassword() {
+ 
+function generatePassword() {
   //Varibles 
-  var completePassword = "";
+  
   var chooseLength = prompt("How many characters do you want your password to have (must be between 8 - 128)?"); 
   var confirmLowercase = confirm("Do you want lowercase letters?");
   var confirmUppercase = confirm("Do you want uppercase letters?");
   var confirmNumeric = confirm("Do you want numeric characters?");
   var confirmSpecial = confirm("Do you want special charactesr?");
 
+  var completePassword = "";
   var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
   var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
   var special = ["~", "%", "!", "}", "{", "|", "'", "`", "_", "^", "]", "[", "/", ":", ";", "=", ">", "<", "?", "@", ".", "-", ",", "+", "*", "#", "$", "&", "("]
- 
+  var newPassword = ""; 
 
    if (chooseLength < 8 || chooseLength > 128) {
      alert("Your password length does not meet security requirements!");
     }
     
    //Password Criteria through prompt:
-     var i;  
-     for(i = 0; i < chooseLength; i++) {
+     
        if (confirmLowercase) {
-         completePassword = completePassword + lowercase[Math.floor(Math.random() * lowercase.length)]; 
+         completePassword = completePassword + lowercase; 
         }
 
         if (confirmUppercase) {
-          completePassword = completePassword + uppercase[Math.floor(Math.random() * uppercase.length)];
+          completePassword = completePassword + uppercase;
           }
 
         if (confirmNumeric) {
-          completePassword = completePassword + numeric[Math.floor(Math.random() * numeric.length)];
+          completePassword = completePassword + numeric;
          }
 
          if (confirmSpecial) {
-          completePassword = completePassword + special[Math.floor(Math.random() * special.length)];
+          completePassword = completePassword + special; 
          }
+        
+        var i;  
+        for(i = 0; i < chooseLength; i++) {
+         newPassword = newPassword + completePassword[Math.floor(Math.random() * completePassword.length)]; 
         }
-      alert("Your new password is: " + completePassword); 
-      console.log(completePassword);
-      return completePassword;
-  }    
 
+        alert("Your new password is: " + newPassword); 
+        return newPassword;
+      
+}    
+//length is too long, its taking at the end 
  // Add event listener to generate button
  generateBtn.addEventListener("click", writePassword); 
 
