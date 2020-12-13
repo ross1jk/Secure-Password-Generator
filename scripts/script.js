8be487d1d701f2d787f03cb4910b9901ac48f74a
Jacqueline Ross <jacqueline.ross09@gmail.com>
2020-12-13 11:27:40 -0500

    changed var for characters from arrays into strings. This removed unecessary commas, no longer have to look at null values

--- scripts/script.js
+++ scripts/script.js
@@ -19,10 +19,10 @@ function generatePassword() {
   var confirmSpecial = confirm("Do you want special charactesr?");
 
   var completePassword = "";
-  var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-  var uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-  var numeric = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-  var special = ["~", "%", "!", "}", "{", "|", "'", "`", "_", "^", "]", "[", "/", ":", ";", "=", ">", "<", "?", "@", ".", "-", ",", "+", "*", "#", "$", "&", "("]
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numeric = "0123456789";
+  var special = "~%!}{|'`_^][/:;=><?@.-+*#$&(";
   var newPassword = ""; 
 
    if (chooseLength < 8 || chooseLength > 128) {
